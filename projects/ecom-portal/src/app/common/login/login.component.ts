453f864584b1722582aae9636e84f6d5d07b9c73
Konstantinos Gkanetsidis <kgkanetsidis@gmail.com>
2021-01-22 15:24:45 +0200

    deployment changes4

--- projects/ecom-portal/src/app/common/login/login.component.ts
+++ projects/ecom-portal/src/app/common/login/login.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../service/login.service';
 import { Router } from '@angular/router';
 import { IUser } from '../service/user';
+import { LoginService } from '../service/login.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EncDecService } from '@lips/common';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
   user: IUser = {
     email: '',
-    password: '',
+    password: ''
   };
-  constructor(
-    private loginService: LoginService,
-    private snackBar: MatSnackBar,
-    private router: Router,
-    private encService: EncDecService
-  ) {}
 
-  ngOnInit(): void {}
+  loginStatus$: Observable<boolean>;
+  constructor(private loginService: LoginService,
+              private snackBar: MatSnackBar,
+              private router: Router,
+              private encService: EncDecService) { }
+
+  ngOnInit() {
+  }
 
   login() {
     this.loginService.login(this.user).subscribe((response) => {
@@ -30,10 +34,12 @@ export class LoginComponent implements OnInit {
         const role = this.encService.encrypt(response.role, '');
         sessionStorage.setItem('role', role);
         sessionStorage.setItem('token', response.data);
+        this.loginService.isLoggedIn(true);
+        this.loginService.userRole(response.role);
         this.navigate(response.role);
       } else {
         this.snackBar.open(response.message, 'Login', {
-          duration: 1000,
+          duration: 1000
         });
       }
     });
@@ -42,15 +48,18 @@ export class LoginComponent implements OnInit {
   navigate(role: string) {
     switch (role) {
       case 'User':
-        // redirect to user
+        // redirect to User
         this.router.navigate(['/user/profile']);
+        break;
       case 'Admin':
         // redirect to Admin
         this.router.navigate(['/admin/dashboard']);
+        break;
       default:
-        this.snackBar.open('User does not have a valid role', 'Login', {
-          duration: 1000,
+        this.snackBar.open('User does not belongs to a valid role!', 'Login', {
+          duration: 1000
         });
     }
   }
+
 }
