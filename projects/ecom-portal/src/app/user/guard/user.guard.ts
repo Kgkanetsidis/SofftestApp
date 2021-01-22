afd0c9c697600ca342d25a2799ad912652b81b96
Konstantinos Gkanetsidis <kgkanetsidis@gmail.com>
2021-01-22 17:04:39 +0200

    routing issues3

--- projects/ecom-portal/src/app/user/guard/user.guard.ts
+++ projects/ecom-portal/src/app/user/guard/user.guard.ts
@@ -1,92 +1,40 @@
 import { Injectable } from '@angular/core';
-import {
-  CanActivate,
-  CanActivateChild,
-  CanDeactivate,
-  CanLoad,
-  Route,
-  UrlSegment,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  UrlTree,
-  Router,
-} from '@angular/router';
-import { EncDecService } from '@lips/common';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { EncDecService } from '@lips/common';
+
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class UserGuard
-  implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+export class UserGuard implements CanActivate, CanLoad {
 
+  constructor(private encService: EncDecService,
+              private router: Router) { }
 
-
-constructor(private encService: EncDecService,
-             private router: Router){}
 
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-
-
-
-      if (sessionStorage.getItem('role') != null) {
-        const role = this.encService.decrypt(sessionStorage.getItem('role'), ' ');
-        if (role === 'User'){
-          return true;
-        }else{
-          this.navigateToLogin(state);
-        }
-      }else{
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (sessionStorage.getItem('role') != null) {
+      const role = this.encService.decrypt(sessionStorage.getItem('role'), '');
+      if (role === 'User') {
+        return true;
+      } else {
         this.navigateToLogin(state);
       }
-
-
-
-      return true;
-  }
-  canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return true;
-  }
-  canDeactivate(
-    component: unknown,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-    return true;
+    } else {
+      this.navigateToLogin(state);
+    }
   }
   canLoad(
     route: Route,
-    segments: UrlSegment[]
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
     return true;
   }
 
-
-  navigateToLogin(state: RouterStateSnapshot){
-    this.router.navigate(['.login', {queryParams: {returnUrl: state.url}}]);
+  navigateToLogin(state: RouterStateSnapshot) {
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
 }
--- projects/ecom-portal/src/app/user/user-routing.module.ts
+++ projects/ecom-portal/src/app/user/user-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-
+import { AuthGuard } from '../common/guard/auth.guard';
 import { UserGuard } from './guard/user.guard';
 import { ProfileComponent } from './profile/profile.component';
 import { ProductDetailsComponent } from './product/product-details/product-details.component';
@@ -12,10 +12,10 @@ import { PaymentComponent } from './payment/payment.component';
 import { ProfileEditComponent } from './profile/profile-edit/profile-edit.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: UserDashboardComponent, canActivate: [ UserGuard] },
-  { path: 'profile', component: ProfileComponent, canActivate: [ UserGuard] },
-  { path: 'profile/edit', component: ProfileEditComponent, canActivate: [ UserGuard] },
-  { path: 'payment', component: PaymentComponent, canActivate: [ UserGuard] }
+  { path: 'dashboard', component: UserDashboardComponent, canActivate: [AuthGuard, UserGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard, UserGuard] },
+  { path: 'profile/edit', component: ProfileEditComponent, canActivate: [AuthGuard, UserGuard] },
+  { path: 'payment', component: PaymentComponent, canActivate: [AuthGuard, UserGuard] }
 ];
 
 @NgModule({
